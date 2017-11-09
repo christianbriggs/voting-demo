20d0e8680225d7185b6abc8650a10548b5b679e5
christianbriggs <briggzay@gmail.com>
2017-11-09 10:34:02 -0500

    Coke vs. Pepsi

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Futbol"
-optionB = "Soccer"
+optionA = "Coke"
+optionB = "Pepsi"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
