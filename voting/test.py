e0b2d4e8a100121393b5da9016ca17bfdc8fb7bf
christianbriggs <briggzay@gmail.com>
2017-11-15 14:25:12 -0500

    updated python test

--- voting/test.py
+++ voting/test.py
@@ -6,12 +6,14 @@ import app
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
+        print('setting up tests')
         self.app = app.app.test_client()
 
     def tearDown(self):
          pass
 
     def test_home_page(self):
+        print('testing home page')
         # Render the / path of the website
         rv = self.app.get('/')
         # Chech that the page contians the desired phrase
